635af1d72209d723f474a958c9cbe7269c6e1d2f
Hugo Valla <Hugo_valla@hotmail.fr>
2020-06-03 19:08:09 +0200

    fetch ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,96 @@
 <template>
   <div class="hello">
     <h1>balalala</h1>
+    <p v-for="(record, index) in records" :key="index">{{ record.Citation }}</p>
+    <p v-for="(record, index) in records" :key="index">{{ record.Auteur }}</p>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 
 export default {
   name: "HelloWorld",
   data() {
     return {
-      apiUrl: "https://api.airtable.com/v0/",
-      apiToken: "keyUzWURiz5JP5KbA", // Always use a read-only account token
-      airTableApp: "appy9OSW6WdE6Dlm0",
-      airTableName: "beige",
-      records: [],
+      fullArray: [],
     };
   },
   mounted() {
-    console.log(process.env.VUE_APP_AIRTABLEKEY);
-    console.log("coucou");
-
     this.getData();
+    console.log(this.fullArray);
   },
   methods: {
     getData: function() {
-      axios
-        .get(
-          `https://api.airtable.com/v0/${this.airTableApp}/${this.airTableName}`,
-          {
-            headers: { Authorization: "Bearer " + this.apiToken },
+      var Airtable = require("airtable");
+      var base = new Airtable({ apiKey: "keyYyXwDnUOjERzzD" }).base(
+        "appy9OSW6WdE6Dlm0"
+      );
+
+      base("Imported table")
+        .select({
+          view: "Grid view",
+        })
+        .eachPage(
+          function page(records) {
+            // This function (`page`) will get called for each page of records.
+
+            records.forEach(function(record) {
+              // console.log(record.fields);
+
+              let fullQuotes = record.fields;
+              console.log(fullQuotes);
+
+              // console.log("Retrieved", record.get("Citations"));
+              // console.log("Retrieved", record.get("Auteur"));
+            });
+
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            // fetchNextPage();
+          },
+          function done(err) {
+            if (err) {
+              console.error(err);
+              return;
+            }
           }
-        )
-        .then((res) => {
-          console.log(res);
-          this.records = res.data.records;
-          console.log(this.records);
-        });
+        );
+      //   let appId = "appy9OSW6WdE6Dlm0";
+      //   let appName = "beige";
+      //   let appKey = "keyspU26TtuKilc1K";
+      //   // let url = "https://api.airtable.com/v0/" + appId + "/" + appName;
+      //   // let axiosConfig = {
+      //   //   headers: {
+      //   //     Authorization: "Bearer " + appKey,
+      //   //     "Content-Type": "application/json",
+      //   //   },
+      //   // };
+      //   axios
+      //     .get("https://api.airtable.com/v0/" + appId + "/" + appName, {
+      //       headers: { Authorization: "Bearer " + appKey },
+      //     })
+      //     .then((resp) => {
+      //       console.log(resp);
+      //     })
+      //     .catch((error) => console.log(error));
     },
   },
 };
+//   axios
+//     .get(
+//       `https://api.airtable.com/v0/${this.airTableApp}/${this.airTableName}`,
+//       {
+//         headers: { Authorization: "Bearer " + this.apiToken },
+//       }
+//     )
+//     .then((res) => {
+//       console.log(res);
+//       this.records = res.data.records;
+//       console.log(this.records);
+//     });
+// },
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
